0ffc85ae0791fc0fdb5d57f44cfd583727eb1413
Mohd Aadil Faizal <mohdaadilf@gmail.com>
2021-05-16 03:48:22 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import requests
+import datetime
+import time
+
+#  IMP
+#  get your token from botfather on Telegram and figure out the chat_id (Can be by sending a message in the created
+#  group and looking at the https://api.telegram.org/bot{token}/getUpdates page.
+
+with open('keys.txt', 'r') as file:
+    token = file.readline().strip('\n')
+    chat_id = file.readline()
+
+# max age
+age = 45
+
+# Print details flag
+print_flag = 'Y'
+
+# Number of days to ahead
+numdays = 7
+
+# district ID
+dist_id = "297"
+
+# header required due to changes to API
+#    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)'
+#               'Chrome/39.0.2171.95 Safari/537.36'}
+
+browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                'Chrome/56.0.2924.76 Safari/537.36'}
+
+#  Getting the dates
+base = datetime.datetime.today()
+date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
+date_str = [x.strftime("%d-%m-%Y") for x in date_list]
+
+# print(base,"\n", date_list, "\n", date_str)
+
+#  for i in range(2):
+program_starts = time.time()
+for INP_DATE in date_str:
+    #  URL for testing -
+    #  URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=
+    #  31-03-2021"
+
+    # API to get planned vaccination sessions on a specific date in a given district.
+    URL = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={dist_id}&" \
+          f"date={INP_DATE}"
+    response = requests.get(URL, headers=browser_header)
+    print(f'Response code: {response.status_code}')
+    if response.ok:
+        resp_json = response.json()
+        # print(json.dumps(resp_json, indent = 1))
+        # flag = False
+        if resp_json["sessions"]:
+            print("Available on: {}".format(INP_DATE))
+            if print_flag == 'y' or print_flag == 'Y':
+                for center in resp_json["sessions"]:  # printing each center
+                    if center["min_age_limit"] <= age:
+                        print("\t", "Name:", center["name"])
+                        print("\t", "Block Name:", center["block_name"])
+                        print("\t", "Pin Code:", center["pincode"])
+                        #   print("\t", "Center:", center)
+                        print("\t", "Min Age:", center['min_age_limit'])
+                        print("\t Free/Paid: ", center["fee_type"])
+                        if center['fee_type'] != "Free":
+                            print("\t", "Amount:", center["fee"])
+                        else:
+                            center["fee"] = '-'
+
+                        print("\t Available Capacity: ", center["available_capacity"])
+                        if center["vaccine"] != '':
+                            print("\t Vaccine: ", center["vaccine"])
+                        else:
+                            center["vaccine"] = '-'
+                        print("\n\n")
+
+                        txt = f'Name:{center["name"]}\nBlock Name:{center["block_name"]}\nPinCode:{center["pincode"]}' \
+                              f'\nMin Age:{center["min_age_limit"]}\nFree/Paid:{center["fee_type"]}\n' \
+                              f'Amount:{center["fee"]}\nAvailable Capacity:{center["available_capacity"]}\n' \
+                              f'Vaccine:{center["vaccine"]}'
+                        to_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=HTML'. \
+                            format(token, chat_id, txt)
+                        resp = requests.get(to_url)
+                        print('Sent')
+        else:
+            print("No available slots on {}".format(INP_DATE))
+    #  time.sleep(25)
+now = time.time()
+print("It has been {} seconds since the loop started".format(now - program_starts))
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Intro
+Initially made into a bot (yet to be deployed) to find Vaccine Centres in Kannur, Kerala for people under the Age 45 (and above 18).
+*  Made using the cowin-api [Click here  for Api Setu Site](https://apisetu.gov.in/public/marketplace/api/cowin)
+* Sends message using telegram. **Must have your own Telegram token and data to send message to telegram**. Refer to [Configuring Telegram Bot](configuring-telegram-bot) or just remove lines 5 to 11 as well as 78 to 85 (shown below):
+```python
+#  IMP
+#  get your token from botfather on Telegram and figure out the chat_id (Can be by sending a message in the created
+#  group and looking at the https://api.telegram.org/bot{token}/getUpdates page.
+
+with open('keys.txt', 'r') as file:
+    token = file.readline().strip('\n')
+    chat_id = file.readline()
+```  
+
+```python
+txt = f'Name:{center["name"]}\nBlock Name:{center["block_name"]}\nPinCode:{center["pincode"]}\nMin Age:{center["min_age_limit"]}\nFree/Paid:{center["fee_type"]}\nAmount:{center["fee"]}\nAvailable Capacity:{center["available_capacity"]}\nVaccine:{center["vaccine"]}'
+to_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=HTML'.format(token, chat_id, txt)
+resp = requests.get(to_url)
+print('Sent')
+``` 
+
+### Configuring Telegram bot
+[Refer to this video]( https://www.youtube.com/watch?v=JBb4-Zeezss).
+Thought the video is a bit long, it does a good job of explaining what needs to be done in order to configure the telegram bot.
+### Modifying for other districts
+* The code can also be modified to check availability in any other district. The code can be modified to check availability of Covid-19 Vaccine for any district in India. To see all district codes, [click on this link and download the CSV file](https://api.covid19india.org/csv/latest/district_wise.csv) . On finding your required district, copy paste it into line number 23 in main.py
+* Fin.
+```python
+dist_id = "INSERT_DISTRICT_IDEA HERE"
+#  For example:
+dist_id = "512
+```
+##### Additional Info
+* Since this is an early build, a lot of random, unwanted code is lying around that may or may not be helpful. Examples include timing the loop and commented code which might come into help whilst creating a bot.
+* [Additional documentation help](https://api.covid19india.org/)
